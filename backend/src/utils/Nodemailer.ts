d3d9f6aef6972e93fe4894d05592259d9e174c6a
Tulio <tutyparis@gmail.com>
2024-10-31 18:37:10 -0300

    nodemailer

--- backend/src/utils/Nodemailer.ts
+++ backend/src/utils/Nodemailer.ts
@@ -3,8 +3,6 @@ import * as nodemailer from 'nodemailer';
 const { EMAIL_SERVICE } = process.env;
 const { ORIGIN_EMAIL } = process.env;
 const { EMAIL_PASS } = process.env;
-const { SMTP_HOST } = process.env;
-const { SMTP_PORT } = process.env;
 
 
 const transporter = nodemailer.createTransport({
