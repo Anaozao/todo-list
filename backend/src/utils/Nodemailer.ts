341f6c94ef2c0d835e3ef4f5cf514c0b0451a7a3
Tulio <tutyparis@gmail.com>
2024-10-31 18:36:37 -0300

    nodemailer

--- backend/src/utils/Nodemailer.ts
+++ backend/src/utils/Nodemailer.ts
@@ -3,10 +3,11 @@ import * as nodemailer from 'nodemailer';
 const { EMAIL_SERVICE } = process.env;
 const { ORIGIN_EMAIL } = process.env;
 const { EMAIL_PASS } = process.env;
+const { SMTP_HOST } = process.env;
+const { SMTP_PORT } = process.env;
+
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
   secure: false,
   service: EMAIL_SERVICE,
   auth: {
